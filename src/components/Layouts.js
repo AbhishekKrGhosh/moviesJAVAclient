fb24e744db7b01cded56e6168ec6ed60094b4712
Abhishek Kumar <abhishek.andri05@gmail.com>
2024-04-04 14:54:02 +0530

    Client side

---
+++ src/components/Layouts.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import React from "react";
+
+function Layouts(){
+    return(
+        <main>
+            <Outlet/>
+        </main>
+)
+}
+
+export default Layouts
